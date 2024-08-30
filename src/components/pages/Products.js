c023ee30588a78b38705239d391670dd445e173c
Doruk HaciHüseyin <dorukhaci08@gmail.com>
2024-08-30 13:09:39 +0300

    ..
    product.css düzenlendi

--- src/components/pages/Products.js
+++ src/components/pages/Products.js
@@ -1,6 +1,6 @@
 import './Products.css';
 import React from "react";
-
+import amarehappyjuicepack from "../images/amarehappyjuicepack.JPG";
 
 const products = [
   {
@@ -8,9 +8,8 @@ const products = [
     name: "Amare Happy Juice™ Pack",
     description: "Award-Winning Daily Gut-Brain Axis Drink to Support Stress Resilience",
     price: "$129.95",
-    
-    imageUrl:`${process.env.PUBLIC_URL}/images/amarehappyjuicepack.jpg`,
-    alt:"amarehappyjuicepack",
+    imageUrl: amarehappyjuicepack,
+    alt: "amarehappyjuicepack",
     buyLink: "https://www.amare.com/1942935/en-us/happy-juice-boosters-pack"
   },
   {
@@ -18,9 +17,8 @@ const products = [
     name: "Amare EDGE+™",
     description: "Advanced Plant-Based Nootropic Drink For Motivation with Lion's Mane and Natural Caffeine",
     price: "$155.95",
-    
-    imageUrl: `${process.env.PUBLIC_URL}/images/AmareEDGE.png` ,
-    alt:"Amare EDGE Product",
+    imageUrl: `${process.env.PUBLIC_URL}/images/AmareEDGE.png`,
+    alt: "Amare EDGE Product",
     buyLink: "https://www.amare.com/1942935/en-us/amareedge-plus-mango-2pack"
   },
   {
@@ -28,31 +26,30 @@ const products = [
     name: "Amare EDGE+™",
     description: "Plant-Based Sugar Free Nootropic Drink for Motivation",
     price: "$155.95",
-   
-    imageUrl:`${process.env.PUBLIC_URL}/images/amareedgegrape.png`,
-    alt:"Amare EDGE+",
+    imageUrl: `${process.env.PUBLIC_URL}/images/amareedgegrape.png`,
+    alt: "Amare EDGE+",
     buyLink: "https://www.amare.com/1942935/en-us/amareedge-plus-grape-2pack"
   },
   {
     id: 4,
     name: "Amare EDGE™ Watermelon [Jar]",
-    description: "Plant-Based Sugar Free Nootropic Drink for Motivation ",
+    description: "Plant-Based Sugar Free Nootropic Drink for Motivation",
     price: "$155.95",
-    
     imageUrl: `${process.env.PUBLIC_URL}/images/edgewatermelon.png`,
     buyLink: "https://www.amare.com/1942935/en-us/amareedge-plus-watermelon-2pack"
   },
-  
 ];
 
 const ProductCard = ({ product }) => (
   <div className="product-card">
     <img src={product.imageUrl} alt={product.name} className="product-image" />
-    <h3>{product.name}</h3>
-    <p>{product.description}</p>
-    <p className="price">
-      <span className="new-price">{product.price}</span>
-    </p>
+    <div className="product-content">
+      <h3>{product.name}</h3>
+      <p>{product.description}</p>
+      <p className="price">
+        <span className="new-price">{product.price}</span>
+      </p>
+    </div>
     <a href={product.buyLink} className="buy-button">BUY NOW</a>
   </div>
 );
