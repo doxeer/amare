217a45cbe457919c17cfce6195b40b0689c72512
FTMustafa <mustafamustafatw@gmail.com>
2024-09-01 15:20:14 +0300

    .
    DORUĞUN HATAYI DÜZELTTİM

--- src/components/pages/Products.js
+++ src/components/pages/Products.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Helmet } from "react-helmet-async";
 import { useNavigate } from "react-router-dom";
+import urunListe from './urunListe';
 import "./Products.css";
-import urunListe from "./urunListe";
 
 function ProductCard({ product }) {
   const navigate = useNavigate();
@@ -12,31 +12,33 @@ function ProductCard({ product }) {
   };
 
   return (
-    <div className="product-card">
+    <div>
       <Helmet>
-        <title>{product.name}</title>
+        <title>{product.name!= 'GBX Protein®' ? `${product.name} - Amare` : 'Amare'}</title>
         <meta name="description" content={product.description} />
         <meta property="og:title" content={product.name} />
         <meta property="og:description" content={product.description} />
         <meta property="og:image" content={product.imageUrl} />
       </Helmet>
-      <div onClick={handleNavigate} style={{ cursor: 'pointer' }}>
-        <img
-          src={product.imageUrl}
-          alt={product.name}
-          className="product-image"
-        />
+      <div className="product-card">
+        <div onClick={handleNavigate} style={{ cursor: "pointer" }}>
+          <img
+            src={product.imageUrl}
+            alt={product.name}
+            className="product-image"
+          />
+        </div>
+        <div className="product-content">
+          <h3>{product.name}</h3>
+          <p>{product.description}</p>
+          <p className="price">
+            <span className="new-price">{product.price}</span>
+          </p>
+        </div>
+        <a href={product.buyLink} className="buy-button">
+          BUY NOW
+        </a>
       </div>
-      <div className="product-content">
-        <h3>{product.name}</h3>
-        <p>{product.description}</p>
-        <p className="price">
-          <span className="new-price">{product.price}</span>
-        </p>
-      </div>
-      <a href={product.buyLink} className="buy-button">
-        BUY NOW
-      </a>
     </div>
   );
 }
