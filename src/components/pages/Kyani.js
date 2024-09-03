97e3306d2ac64d3597a0ae244bd25e6a2e4a2b13
Doruk HaciHüseyin <dorukhaci08@gmail.com>
2024-09-04 00:25:44 +0300

    ..
    3 ürün yükledim kyani tarafina devam edilecek yoruldum.

--- src/components/pages/Kyani.js
+++ src/components/pages/Kyani.js
@@ -1,40 +1,67 @@
 import React from "react";
 import "./Kyani.css";
 import { Helmet } from "react-helmet-async";
+import kyanisunset from "../images/Kyanisunset.png"
+import Kyaniberrylemonade from "../images/Kyaniberrylemonade.png"
+import kyanipeach from "../images/Kyanipeach.png"
+
 
 const products = [
   {
     id: 1,
-    name: "GBX Pep™ [12 Cans]",
+    name: "Kyäni Sunset",
     description:
-      "Why You'll Love It Supports mental alertness and improved concentration.",
-    price: "$35.95",
-
-    imageUrl: "https://via.placeholder.com/150",
-    buyLink: "https://example.com/buy-gbx-pep",
+      "Kyäni Sunset is your go-to for supporting your most important organs---your brain and your heart---while also helping maintain healthy cholesterol levels, cognitive function, immune support, and healthy blood sugar levels. Featuring a proprietary blend of lipid-soluble nutrients including wild-sourced omega-3 fatty acids, tocotrienols, astaxanthin, and more, Kyäni Sunset is the nightly routine that helps you wake up ready to slay your day!",
+    price: "$49.95",
+    imageUrl: kyanisunset,
+    alt: "kyanisunset",
+    buyLink: "https://www.amare.com/1942935/en-us/kyani-sunset",
+    linkName: "kyani-sunset",
   },
   {
     id: 2,
-    name: "Digestive",
+    name: "Kyäni HL5 Berry Lemonade",
     description:
-      "Features & Benefits Delivers a balanced and comprehensive blend of digestive enzymes.",
-    price: "$31.95",
-
-    imageUrl: "https://via.placeholder.com/150",
-    buyLink: "https://example.com/buy-digestive",
+      "Our game-changing collagen support formula, Kyäni HL5, contains clean-sourced, hydrolyzed collagen protein and other premium ingredients to support collagen resiliency to help us to look younger, feel younger, and move younger in a convenient on-the-go pouch",
+    price: "$77.95",
+    imageUrl: Kyaniberrylemonade,
+    alt: "kyanihl5berrylemonade",
+    buyLink: "https://www.amare.com/1942935/en-us/kyani-hl5-berry",
+    linkName: "kyani-hl5-berry-lemonade",
   },
   {
     id: 3,
-    name: "Amare EDGE™ Grape [Stick Packs]",
+    name: "Kyäni HL5 Peach",
     description:
-      "Cutting edge gut-brain axis science packed into a refreshing strawberry cream beverage.",
-    price: "$78.95",
-
-    imageUrl: "https://via.placeholder.com/150",
-    buyLink: "https://example.com/buy-edge-grape",
+      "Our game-changing collagen support formula, Kyäni HL5, contains clean-sourced, hydrolyzed collagen protein and other premium ingredients to support collagen resiliency to help us to look younger, feel younger, and move younger in a convenient on-the-go pouch.*",
+    price: "$77.95",
+    alt: "kyanihl5berrypeach",
+    imageUrl: kyanipeach ,
+    buyLink: "https://www.amare.com/1942935/en-us/kyani-hl5-peach",
+    linkName: "kyani-hl5-peach",
   },
   {
     id: 4,
+    name: "Amare EDGE™ Watermelon [Jar]",
+    description:
+      "Amare EDGE, the all-natural nootropic, that helps give you the motivation to t.",
+    price: "$78.95",
+
+    imageUrl: "https://via.placeholder.com/150",
+    buyLink: "https://example.com/buy-edge-watermelon",
+  },
+  {
+    id: 5,
+    name: "Amare EDGE™ Watermelon [Jar]",
+    description:
+      "Amare EDGE, the all-natural nootropic, that helps give you the motivation to t.",
+    price: "$78.95",
+
+    imageUrl: "https://via.placeholder.com/150",
+    buyLink: "https://example.com/buy-edge-watermelon",
+  },
+  {
+    id: 6,
     name: "Amare EDGE™ Watermelon [Jar]",
     description:
       "Amare EDGE, the all-natural nootropic, that helps give you the motivation to t.",
