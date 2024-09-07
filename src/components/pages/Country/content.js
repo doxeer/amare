110fe8db8179a904be4ba5175ba1993a0a10967d
FTMustafa <mustafamustafatw@gmail.com>
2024-09-07 15:39:10 +0300

    .
    Ufak link düzeltmeleri

--- src/components/pages/Country/content.js
+++ src/components/pages/Country/content.js
@@ -24,7 +24,7 @@ const content = {
         description: "Register with Amare from Canada and have a business operating in 48 countries.",
         readyText: "Ready to get started? <a href='https://amarewellnessproducts.com/create-an-amare-brand-partner-account-from/canada'>Click here to register now.</a>",
     },
-    czech_republic: {
+    'czech-republic': {
         title: "Vytvořte si účet Amare Brand Partner z České Republiky",
         description: "Zaregistrujte se u Amare z České Republiky a provozujte podnikání ve 48 zemích.",
         readyText: "Připraveni začít? <a href='https://amarewellnessproducts.com/create-an-amare-brand-partner-account-from/czech_republic'>Klikněte zde pro registraci nyní.</a>",
@@ -54,7 +54,7 @@ const content = {
         description: "Regístrese en Amare desde México y opere en 48 países.",
         readyText: "¿Listo para comenzar? <a href='https://amarewellnessproducts.com/create-an-amare-brand-partner-account-from/mexico'>Haga clic aquí para registrarse ahora.</a>",
     },
-    new_zealand: {
+    'new-zealand': {
         title: "Create an Amare Brand Partner Account from New Zealand",
         description: "Register with Amare from New Zealand and have a business operating in 48 countries.",
         readyText: "Ready to get started? <a href='https://amarewellnessproducts.com/create-an-amare-brand-partner-account-from/new_zealand'>Click here to register now.</a>",
@@ -74,12 +74,12 @@ const content = {
         description: "Amare'ye Türkiye'den kaydolun ve 48 ülkede iş yapın.",
         readyText: "Başlamak için hazır mısınız? <a href='https://amarewellnessproducts.com/create-an-amare-brand-partner-account-from/turkey'>Şimdi kaydolmak için buraya tıklayın.</a>",
     },
-    'united_states': {
+    'united-states': {
         title: "Create an Amare Brand Partner Account from the United States",
         description: "Register with Amare from the United States and have a business operating in 48 countries.",
         readyText: "Ready to get started? <a href='https://amarewellnessproducts.com/create-an-amare-brand-partner-account-from/united_states'>Click here to register now.</a>",
     },
-    united_kingdom: {
+    'united-kingdom': {
         title: "Create an Amare Brand Partner Account from the United Kingdom",
         description: "Register with Amare from the United Kingdom and have a business operating in 48 countries.",
         readyText: "Ready to get started? <a href='https://amarewellnessproducts.com/create-an-amare-brand-partner-account-from/united_kingdom'>Click here to register now.</a>",
